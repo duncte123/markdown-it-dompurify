2c017ceca7f9c94f929979281cc7cfd82f3044cd
duncte123 <contact@duncte123.me>
2022-02-10 19:21:27 +0100

    aaaaaa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 module.exports = function markdownitDommpurify () {
-    const lib = require('isomorphic-dompurify');
-    const createDOMPurify = lib && lib.default || lib;
-    const dompurify = createDOMPurify();
-
-    if (!dompurify.isSupported) {
-        throw new Error('Dompurify is not supported in your current environment.');
-    }
+    const DOMPurify = require('isomorphic-dompurify');
 
     return function dompurifyPlugin (md) {
         md.core.ruler.before('normalize', 'purify_dom', function purify_dom(state) {
-            if (dompurify.isSupported) {
-                state.src = dompurify.sanitize(state.src);
-            }
+            state.src = DOMPurify.sanitize(state.src);
         });
     }
 };
