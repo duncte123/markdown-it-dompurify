5e3d86b176e5b36f1246a278d3f18981faf13cd2
duncte123 <contact@duncte123.me>
2022-02-10 19:26:04 +0100

    And another thing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-module.exports = function markdownitDommpurify () {
-    const DOMPurify = require('isomorphic-dompurify');
+const DOMPurify = require('isomorphic-dompurify');
 
-    return function dompurifyPlugin (md) {
-        md.core.ruler.before('normalize', 'purify_dom', function purify_dom(state) {
-            state.src = DOMPurify.sanitize(state.src);
-        });
-    }
+module.exports = function dompurifyPlugin (md) {
+    md.core.ruler.before('normalize', 'purify_dom', function purify_dom(state) {
+        state.src = DOMPurify.sanitize(state.src);
+    });
 };
