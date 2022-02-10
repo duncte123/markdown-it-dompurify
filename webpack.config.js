5fee11e1fc189d82aec091dd1bfdea66bb447c26
duncte123 <contact@duncte123.me>
2022-02-10 18:48:09 +0100

    Attempt commonjs as target

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     target: 'web',
     entry: './src/index.js',
     output: {
+        libraryTarget: 'commonjs2',
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
